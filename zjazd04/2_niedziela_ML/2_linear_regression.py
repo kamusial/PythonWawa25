4afe37e0952ee3e28fd0cce6a462e1a15a8859ce
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-01-26 11:20:54 +0100

    'a'

--- zjazd04/2_niedziela_ML/2_linear_regression.py
+++ zjazd04/2_niedziela_ML/2_linear_regression.py
@@ -3,3 +3,6 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
 
+df = pd.read_csv('data\\otodom.csv')
+print(df)
+print(df.describe().T.to_string())
