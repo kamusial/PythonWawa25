446657e73c40e31c6c40eadeda0b4bde67eb3e51
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-01-26 09:59:02 +0100

    'a'

--- zjazd04/2_niedziela_ML/1_linear_regression.py
+++ zjazd04/2_niedziela_ML/1_linear_regression.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv('weight-height.csv')
 print(df)
@@ -7,3 +9,22 @@ print(df.head(10))
 df.Height *= 2.54
 df.Weight /= 2.2
 print(df.describe())
+
+# plt.hist(df.Weight)
+# plt.show()
+# plt.hist(df.query("Gender=='Male'").Weight, bins=30)
+# plt.hist(df.query("Gender=='Female'").Weight, bins=30)
+# plt.show()
+# sns.histplot(df.Weight)
+# plt.show()
+# sns.histplot(df.query("Gender=='Male'").Weight)
+# sns.histplot(df.query("Gender=='Female'").Weight)
+# plt.show()
+
+# wejściowe, dane niezależne -> gender, height
+# wyjście, dane zaleźne -> weight
+
+df = pd.get_dummies(df)
+print(df)
+del (df['Gender_Male'])
+print(df)
