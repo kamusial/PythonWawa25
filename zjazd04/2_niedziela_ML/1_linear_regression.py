ae2d6ec8564c9f211651f6048677c2351f4271e2
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-01-26 11:03:16 +0100

    'a'

--- zjazd04/2_niedziela_ML/1_linear_regression.py
+++ zjazd04/2_niedziela_ML/1_linear_regression.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv('weight-height.csv')
 print(df)
@@ -23,8 +24,25 @@ print(df.describe())
 
 # wejściowe, dane niezależne -> gender, height
 # wyjście, dane zaleźne -> weight
-
+print('get_dummies - zamieniam dane nienumeryczne na numeryczne')
 df = pd.get_dummies(df)
 print(df)
+print('usuwam kolumnę Gender_Male')
 del (df['Gender_Male'])
 print(df)
+print('Zmieniam nazwę kolumny')
+df.rename(columns={'Gender_Female': 'Gender'}, inplace=True)
+print(df)
+print('Dane gotowe')
+# gender 0 - facet, 1 - kobieta
+
+model = LinearRegression()
+model.fit( df[['Height', 'Gender']]   ,  df['Weight']  )
+print(f'Wspóczynnik kierunkowy: {model.coef_}\nwyraz wolny: {model.intercept_}')
+
+height = 192
+gender = 1
+weight = 1.06 * height -8.8 * gender - 102.5
+print(weight)
+
+print(model.predict([[192, 0], [167, 1], [10, 1]]))
