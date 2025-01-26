2d2921d35bc3bba58acc3adf2cba279384313c17
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-01-26 11:05:45 +0100

    'a'

--- zjazd04/2_niedziela_ML/1_linear_regression.py
+++ zjazd04/2_niedziela_ML/1_linear_regression.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
 
-df = pd.read_csv('weight-height.csv')
+df = pd.read_csv('data//weight-height.csv')
 print(df)
 print(df.head(10))
 
---
+++ zjazd04/2_niedziela_ML/2_linear_regression.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+
