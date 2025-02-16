d99c0f323837e253dd72db22e25a9ee518c24e0c
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-02-16 11:32:29 +0100

    '1'

--- zjazd05/2_niedziela_UX/1_Tkinter/8_pole_tekstowe.py
+++ zjazd05/2_niedziela_UX/1_Tkinter/8_pole_tekstowe.py
@@ -1,28 +1,32 @@
 import tkinter as tk
 from tkinter.messagebox import showinfo
+import customtkinter as ctk
 
-def clicked1():
+def clicked_entry():
     msg = f'Wpisałeś: {entry_text.get()}'
-    showinfo(title='Info', message=msg)
+    showinfo(title='Pole ENTRY', message=msg)
     print('Kliknęto')
     print(f'Wpisano {entry_text.get()}')
 
+def clicked_text():
+    # Pobiera tekst od początku ("1.0") do końca (tk.END)
+    content = text_field.get('1.0', tk.END)
+    showinfo(title='POte tekstowe TEXT', message=content)
 
-
-root = tk.Tk()
+root = ctk.CTk()   # główne okno Custom Tkinter
 entry_text = tk.StringVar()
 
 frame = tk.Frame()
 frame.pack(padx=50, pady=50)
 label = tk.Label(frame, text='Wpisz cos')
 label.pack()
-entry = tk.Entry(frame, textvariable=entry_text, font=('Helvetica', 20))
+entry = tk.Entry(frame, textvariable=entry_text, show='*', font=('Helvetica', 20))
 entry.pack(ipadx=100, ipady=30)
 text_field = tk.Text(frame, height=3, width=40)
 text_field.pack()
-button1 = tk.Button(frame, text='Entry', command=clicked1)
-button1.pack()
-button2 = tk.Button(frame, text='Text', command=clicked2)
+button1 = tk.Button(frame, text='Entry', command=clicked_entry)
+button1.pack(fill='x', ipady=5, pady=5)
+button2 = tk.Button(frame, text='Text', command=clicked_text)
 button2.pack()
 
 root.mainloop()
