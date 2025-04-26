8cba0c538034b3c90a18b8a9cf6973eb1ed223da
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-04-26 12:16:24 +0200

    1

--- zjazd7_ML/2_knn.py
+++ zjazd7_ML/2_knn.py
@@ -28,3 +28,26 @@ df["distance"] = (df.sepallength-sample[0])**2 + (df.sepalwidth-sample[1])**2 +\
 
 print(df.head(5).to_string())
 print(df.sort_values('distance').head(7).to_string())
+
+
+
+print('Algorytm KNN')
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+X = df.iloc[:, :4]
+y = df.class_value
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
+model = KNeighborsClassifier(n_neighbors=3, weights='uniform')
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame( confusion_matrix(y_test, model.predict(X_test)  ) ))
+
+result = []
+for k in range(1, 101):
+    model = KNeighborsClassifier(n_neighbors=k, weights='uniform')
+    model.fit(X_train, y_train)
+    result.append( model.score(X_test, y_test) )
+
+plt.plot(range(1, 101), result)
+plt.show()
