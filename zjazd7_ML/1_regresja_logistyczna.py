7dcbc4db530b125799f42d676c67c50b5bd73d0d
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-04-26 09:59:58 +0200

    1

--- zjazd7_ML/1_regresja_logistyczna.py
+++ zjazd7_ML/1_regresja_logistyczna.py
@@ -2,6 +2,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
+import numpy as np
 
 df = pd.read_csv('data//diabetes.csv')
 print(df.describe().T.round(2).to_string())
@@ -17,3 +18,8 @@ print(df.outcome.value_counts())
 
 for col in ['glucose', 'bloodpressure', 'skinthickness', 'insulin',
        'bmi', 'diabetespedigreefunction', 'age']:
+    df[col] = df[col].replace(0, np.nan)   # wcześniej np.NaN
+    mean_ = df[col].mean()
+    df[col].replace(np.nan, mean_, inplace=True)   # niedługo niewspierane
+print('Po czyszczeniu danych')
+print(df.describe().T.round(2).to_string())
