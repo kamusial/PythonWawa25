db4d4dc49a3b80eb1853dd808154a7355d34da26
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-04-26 14:55:03 +0200

    1

--- zjazd7_ML/7_Neural1a.py
+++ zjazd7_ML/7_Neural1a.py
@@ -5,8 +5,8 @@ from tensorflow.keras.layers import Dense
 
 model = Sequential()
 model.add(Dense(1, activation='linear'))
-model.add(Dense(4, activation='relu'))
-model.add(Dense(4, activation='linear'))
+model.add(Dense(20, activation='relu'))
+model.add(Dense(14, activation='linear'))
 # model.add(Dense(4, activation='linear'))
 model.add(Dense(1, activation='linear'))
 # liczba warstw, liczna neuronów w warstwie
@@ -15,7 +15,7 @@ model.compile(optimizer='rmsprop', loss='mse')
 
 df = pd.read_csv('data\\f-c.csv', usecols=[1, 2])
 
-result = model.fit(df.F, df.C, epochs=100, verbose=2)
+result = model.fit(df.F, df.C, epochs=400, verbose=2)
 # liczba epok
 
 C_pred = model.predict(df.F)
