7a0baba33c8431d9f53866c8fa418847e3a2d80f
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-04-26 13:29:04 +0200

    'q'

---
+++ zjazd7_ML/4_las_losowy.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("data\\iris.csv")
+print(df['class'].value_counts())
+
+species = {
+    'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2
+}
+
+df['class_value'] = df['class'].map(species)
+print(df['class_value'].value_counts())
+sample = [5.6, 3.2, 5.2, 1.45]
+
+# plt.scatter(df.sepallength, df.sepalwidth)
+# sns.scatterplot(df, x='sepallength', y='sepalwidth', hue='class')
+# plt.scatter(5.6, 3.2, c='r')
+# plt.show()
+# sns.scatterplot(df, x='petallength', y='petalwidth', hue='class')
+# plt.scatter(5.2, 1.45, c='r')
+# plt.show()
+
+
+print('Algorytm Las Losowy')
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+X = df.iloc[:, 0:4]
+y = df.class_value
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
+model = RandomForestClassifier(n_estimators=100, criterion='gini', max_depth=None, min_samples_split=2, verbose=2)
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame( confusion_matrix(y_test, model.predict(X_test)  ) ))
+print(pd.DataFrame( model.feature_importances_ , X.columns))
+
+# from mlxtend.plotting import plot_decision_regions
+# plot_decision_regions(X.values, y.values, model)
+# plt.show()
