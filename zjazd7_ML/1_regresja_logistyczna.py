ec08d5bb2e60864222625905302ee827e2284b69
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-04-26 10:49:37 +0200

    1

--- zjazd7_ML/1_regresja_logistyczna.py
+++ zjazd7_ML/1_regresja_logistyczna.py
@@ -23,3 +23,31 @@ for col in ['glucose', 'bloodpressure', 'skinthickness', 'insulin',
     df[col].replace(np.nan, mean_, inplace=True)   # niedługo niewspierane
 print('Po czyszczeniu danych')
 print(df.describe().T.round(2).to_string())
+
+# df.to_csv("data\\cukrzyca.csv", index=False)
+
+X = df.iloc[:,:-1]
+y = df.outcome
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame( confusion_matrix(y_test, model.predict(X_test) ) ))
+
+print('Zmiana danych')
+df1 = df.query("outcome==0").sample(n=500)
+df2 = df.query("outcome==1").sample(n=500)
+df3 = pd.concat([df1, df2])  # 1000 próbek
+
+X = df3.iloc[:,:-1]
+y = df3.outcome
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame( confusion_matrix(y_test, model.predict(X_test) ) ))
