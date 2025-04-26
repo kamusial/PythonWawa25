65f7c32927b1d3d54e7b119611c0a09378d0ccfd
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-04-26 13:15:52 +0200

    1

--- zjazd7_ML/3_drzewa_decyzyjne.py
+++ zjazd7_ML/3_drzewa_decyzyjne.py
@@ -15,34 +15,27 @@ print(df['class_value'].value_counts())
 sample = [5.6, 3.2, 5.2, 1.45]
 
 # plt.scatter(df.sepallength, df.sepalwidth)
-sns.scatterplot(df, x='sepallength', y='sepalwidth', hue='class')
-plt.scatter(5.6, 3.2, c='r')
-plt.show()
-sns.scatterplot(df, x='petallength', y='petalwidth', hue='class')
-plt.scatter(5.2, 1.45, c='r')
-plt.show()
+# sns.scatterplot(df, x='sepallength', y='sepalwidth', hue='class')
+# plt.scatter(5.6, 3.2, c='r')
+# plt.show()
+# sns.scatterplot(df, x='petallength', y='petalwidth', hue='class')
+# plt.scatter(5.2, 1.45, c='r')
+# plt.show()
 
 
 print('Algorytm Drzewa decyzyjne')
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
-X = df.iloc[:, :2]
+X = df.iloc[:, 0:4]
 y = df.class_value
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
-model = DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None, random_state=None, max_leaf_nodes=None, min_impurity_decrease=0.0, class_weight=None, ccp_alpha=0.0, monotonic_cst=None)
+model = DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=20, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None, random_state=None, max_leaf_nodes=None, min_impurity_decrease=0.0, class_weight=None, ccp_alpha=0.0, monotonic_cst=None)
 model.fit(X_train, y_train)
 print(model.score(X_test, y_test))
 print(pd.DataFrame( confusion_matrix(y_test, model.predict(X_test)  ) ))
+print(pd.DataFrame( model.feature_importances_ , X.columns))
 
-from mlxtend.plotting import plot_decision_regions
-
-plt.figure(figsize=(7,7))
-plot_decision_regions(X.values, y.values, model)
-
-
-from dtreeplt import dtreeplt
-
-dtree = dtreeplt(model=model, feature_names=X.columns, target_names=["setosa","versicolor","virginica"])
-dtree.view()
-plt.show()
+# from mlxtend.plotting import plot_decision_regions
+# plot_decision_regions(X.values, y.values, model)
+# plt.show()
