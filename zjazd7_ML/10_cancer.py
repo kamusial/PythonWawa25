5b859aa27ac05ea7e7b4e7a3c4c729dfcfb234cc
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-04-27 12:02:49 +0200

    1

---
+++ zjazd7_ML/10_cancer.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.random import seed
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from collections import Counter
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+### Pandas - ustawienia
+pd.set_option('display.max_rows', 5000)
+pd.set_option('display.max_columns', 5000)
+pd.set_option('display.width', 10000)
+pd.set_option('display.max_colwidth', 10000)
+np.set_printoptions(linewidth=2000)
+
+
+X, y =load_breast_cancer(return_X_y=True, as_frame=True)
+print(X.shape)
+print(X.columns)
+print(X.describe())
+print(Counter(y))
+
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+# model = MLPClassifier(hidden_layer_sizes=(100, 100, 100, 100, 100), max_iter=1000, activation='relu')
+# model.fit(X_train, y_train)
+# y_pred = model.predict(X_test)
+# print(pd.DataFrame(confusion_matrix(y_test, y_pred)))
+# print(accuracy_score(y_test, y_pred))
+# print(model.n_layers_)
+
--- zjazd7_ML/9_pingwiny.py
+++ zjazd7_ML/9_pingwiny.py
@@ -19,13 +19,17 @@ X_train, X_test, y_train, y_test = train_test_split(penguins_features, penguins_
 
 from tensorflow import keras
 inputs = keras.Input(shape=[X_train.shape[1]])
-hidden_layer1 = keras.layers.Dense(10, activation='relu')(inputs)
-hidden_layer2 = keras.layers.Dense(10, activation='linear')(hidden_layer1)
-output_layer = keras.layers.Dense(3, activation='softmax')(hidden_layer2)
+hidden_layer1 = keras.layers.Dense(20, activation='relu')(inputs)
+hidden_layer2 = keras.layers.Dense(20, activation='linear')(hidden_layer1)
+hidden_layer3 = keras.layers.Dense(20, activation='linear')(hidden_layer2)
+hidden_layer4 = keras.layers.Dense(20, activation='linear')(hidden_layer3)
+hidden_layer5 = keras.layers.Dense(20, activation='linear')(hidden_layer4)
+hidden_layer6 = keras.layers.Dense(20, activation='linear')(hidden_layer5)
+output_layer = keras.layers.Dense(3, activation='softmax')(hidden_layer6)
 
 model = keras.Model(inputs=inputs, outputs=output_layer)
 model.compile(optimizer='adam', loss=keras.losses.CategoricalCrossentropy())
-result = model.fit(X_train, y_train, epochs=100)
+result = model.fit(X_train, y_train, epochs=1000)
 # print(result.history)
 sns.lineplot(x=result.epoch, y=result.history['loss'])
 plt.show()
