646f08316b116955a2db1e9eefb686a0108a9723
Michał Szajkowski <myjniak@gmail.com>
2025-05-24 14:16:10 +0200

    complete

--- zjazd09/1_sobota/invoice_generator_project/invoice_generator/fonts.py
+++ zjazd09/1_sobota/invoice_generator_project/invoice_generator/fonts.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from fpdf import FPDF
+from fpdf.enums import TextEmphasis
 
 
 def create(pdf: FPDF):
@@ -9,14 +10,21 @@ def create(pdf: FPDF):
     # pathib.Path approach:
     arial_path = Path(__file__).parent / "fonts" / "ARIAL.ttf"
     arial_italic_path = Path(__file__).parent / "fonts" / "ARIALI.TTF"
+    arial_bold_path = Path(__file__).parent / "fonts" / "ARIALBD.TTF"
 
     pdf.core_fonts_encoding = "utf-8"
     pdf.add_font("arial", style="I", fname=arial_italic_path)
     pdf.add_font("arial", style="", fname=arial_path)
+    pdf.add_font("arial", style="B", fname=arial_bold_path)
 
 
 def normal(pdf: FPDF, size: int = 6):
     pdf.set_font('arial', size=size)
+
+
+def bold(pdf: FPDF, size: int = 6):
+    pdf.set_font('arial', style="B", size=size)
+
 
 
 def italic(pdf: FPDF, size: int = 6):
--- zjazd09/1_sobota/invoice_generator_project/invoice_generator/main.py
+++ zjazd09/1_sobota/invoice_generator_project/invoice_generator/main.py
@@ -11,20 +11,20 @@ def header(pdf: FPDF, config: InvoiceConfig):
     pdf.ln(10)
     fonts.italic(pdf, 10)
     pdf.cell(text="Sprzedawca/Podatnik: ")
-    fonts.normal(pdf, 8)
+    fonts.normal(pdf, 6)
     pdf.cell(w=0, text=f"{config.company.city}, {datetime.today().strftime("%Y-%m-%d")}", align="R")
     pdf.ln(5)
     pdf.cell(text=config.company.name)
-    pdf.ln(5)
+    pdf.ln(3)
     pdf.cell(text=config.company.address)
-    pdf.ln(5)
+    pdf.ln(3)
     pdf.cell(text=f"{config.company.postal_code}, {config.company.city}")
     fonts.italic(pdf, size=6)
-    pdf.ln(5)
+    pdf.ln(3)
     pdf.cell(text="NIP: ")
-    fonts.normal(pdf, 8)
+    fonts.normal(pdf, 6)
     pdf.cell(text=config.company.nip)
-    pdf.ln(5)
+    pdf.ln(3)
     fonts.normal(pdf, 8)
     pdf.cell(w=0, text=f"Bank: {config.account.bank_name}", align="R")
     pdf.ln(5)
@@ -49,6 +49,34 @@ def buyer(pdf: FPDF, config: InvoiceConfig):
     buyer_asset(pdf, "Sposób zapłaty: ", config.invoice.payment_method)
     paid_until = datetime.today() + timedelta(days=config.invoice.payment_period)
     buyer_asset(pdf, "Płatne do dnia: ", paid_until.strftime("%Y-%m-%d"))
+    pdf.ln(10)
+
+
+def table_cell(pdf: FPDF, w: int, text: str, new_x="RIGHT", new_y="LAST"):
+    pdf.multi_cell(w=w, border=1, padding=2, align="C", new_x=new_x, new_y=new_y, text=text)
+    pdf.y -= 2
+
+def items(pdf: FPDF, config: InvoiceConfig):
+    fonts.normal(pdf, size=8)
+    table_cell(pdf, 100, "Nazwa")
+    table_cell(pdf, 20, "% VAT")
+    table_cell(pdf, 30, "Cena NETTO")
+    table_cell(pdf, 30, "Cena BRUTTO", new_x="LMARGIN", new_y="NEXT")
+    pdf.y += 2
+
+    total = 0
+    for item in config.items:
+        table_cell(pdf, 100, item.name)
+        table_cell(pdf, 20, str(item.tax))
+        table_cell(pdf, 30, f"{'%.2f' % item.net} {config.account.currency}")
+        gross = item.net * (1 + item.tax / 100)
+        total += gross
+        table_cell(pdf, 30, f"{"%.2f" % round(gross, 2)} {config.account.currency}",  new_x="LMARGIN", new_y="NEXT")
+        pdf.y += 2
+
+    pdf.ln(10)
+    fonts.bold(pdf, size=15)
+    pdf.cell(text=f"Do zapłaty: {"%.2f" % round(total, 2)} {config.account.currency}")
 
 
 def main():
@@ -60,6 +88,7 @@ def main():
     fonts.normal(pdf, 12)
     header(pdf, config)
     buyer(pdf, config)
+    items(pdf, config)
 
     pdf.output("invoice.pdf")
 
