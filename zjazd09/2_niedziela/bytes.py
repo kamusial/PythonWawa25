1b04f21b8b4e285bf10ef43fbdb0c96e44aa39de
Michał Szajkowski <myjniak@gmail.com>
2025-05-25 11:27:57 +0200

    pokemon oczko

---
+++ zjazd09/2_niedziela/bytes.py
@@ -0,0 +1,4 @@
+string = "안녕"
+print(string)
+string_as_bytes = string.encode("UTF-8")
+print(string_as_bytes)
--- zjazd09/2_niedziela/pokeweight_blackjack.py
+++ zjazd09/2_niedziela/pokeweight_blackjack.py
@@ -7,3 +7,50 @@ Jeśli powiemy "tak" i skończy nam się miejsce w walizce - PRZEGRYWAMY.
 Jeśli powiemy "nie", gra się kończy i printuje pokemony, które
 zabieramy ze sobą do domu ^_^
 """
+
+import httpx
+import random
+from dataclasses import dataclass
+
+
+# class Pokemon:
+#     def __init__(self, name, weight):
+#         self.name = name
+#         self.weight = weight
+
+
+@dataclass
+class Pokemon:
+    name: str
+    weight: int
+
+
+def get_random_pokemon():
+    pokemon_id = random.randint(1, 151)
+    response = httpx.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}")
+    pokemon_data = response.json()
+    return Pokemon(pokemon_data["name"], pokemon_data["weight"])
+
+
+def main():
+    pokemon = get_random_pokemon()
+    suitcase = pokemon.weight
+    pokemons_in_suitcase = [pokemon.name]
+    print(f"You found {pokemon.name}! Suitcase load: {suitcase}/2000")
+    while input("Do you want to take another Pokemon with you? (y/n): ") == "y":
+        pokemon = get_random_pokemon()
+        suitcase += pokemon.weight
+        pokemons_in_suitcase.append(pokemon.name)
+        if suitcase > 2000:
+            print(f"You found {pokemon.name} (weight {pokemon.weight})! Suitcase overloaded! You lost! :(")
+            break
+        print(f"You found {pokemon.name}! Suitcase load: {suitcase}/2000")
+    else:
+        print(f"Contrats, you took {', '.join(pokemons_in_suitcase)} with you :)")
+
+
+
+if __name__ == '__main__':
+    main()
+
+
